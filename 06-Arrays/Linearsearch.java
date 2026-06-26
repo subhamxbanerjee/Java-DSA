591e998336deef7896655a1ee55ba93881379ebd
theultimate-gif <theuiltimate036@gmail.com>
2026-06-26 17:08:09 +0530

    executed linear search algorithm

---
+++ 06-Arrays/Linearsearch.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Linearsearch {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter array size");
+        int n =sc.nextInt();
+        int [] arr= new int[n];
+        System.out.println("enter the array elements:");
+        for(int i=0;i<arr.length;i++){
+            arr[i]=sc.nextInt();
+        }
+        System.out.println("enter the numbet to search:");
+        int x =sc.nextInt();
+        for(int i=0;i<n;i++){
+            if(arr[i]==x){
+                System.out.println("elements found at index "+i);
+                return;
+            }
+        }
+        System.out.println("elements noot found");
+    }
+    
+}
